c0c104ca8a2070e81f5eba858e1ba07315b20868
alumno <alumno@utnfra.com.ar>
2019-05-07 21:32:27 -0300

    commit parcial 2

---
+++ 20190507 – Camela.Nestor_2/Entidades/PlacaVideo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class PlacaVideo:Periferico
+    {
+        private int ramMB;
+        
+        public PlacaVideo(string marca, string modelo, EConector conector,int ramMB) : base(marca, modelo, conector)
+        {
+            this.ramMB = ramMB;
+        }
+
+
+
+
+        public override string ExponerDatos()
+        {
+            return "aaa";
+        }
+
+     
+    }
+}
